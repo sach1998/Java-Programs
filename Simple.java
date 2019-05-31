2b8538355c4ab631a9b29466d5d9c7997e44a04b
Sachin Koparde <navodayasacchu26@gmail.com>
2019-05-31 21:00:50 +0530

    Add files via upload

---
+++ Simple.java
@@ -0,0 +1,19 @@
+package myPackage;
+
+
+interface quick
+{
+	public void heal();
+}
+
+public class Simple 
+{
+	public static void main(String[] args) 
+	{
+		quick q=()->
+		{
+			System.out.println("Heal bad thoughts");
+		};
+		q.heal();
+	}
+}
